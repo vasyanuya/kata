7557d4fd6bec2b8b80503aedf25f683b20651da0
vasyanuya <150078042+vasyanuya@users.noreply.github.com>
2023-11-14 13:22:07 +0300

    Update Calculator.java

--- calc/src/Calculator.java
+++ calc/src/Calculator.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, IllegalArgumentException, ArrayIndexOutOfBoundsException {
         Scanner input = new Scanner(System.in);
         Main result = new Main();
         System.out.println("Введите выражение:");
@@ -12,53 +13,56 @@ public class Calculator {
     }
 }
 class Main{
-    public static String calc(String input){
-        String exception = "Exception";
+    public static String calc(String input) throws IOException, IllegalArgumentException, ArrayIndexOutOfBoundsException {
+
         String[] arr = input.split(" ");
         if (arr.length != 3) {
-            return exception;
+            throw new IOException();
         }
-
         int num1;
         int num2;
         boolean isRoman = false;
-        try {
+        Scanner in1 = new Scanner(arr[0]);
+        boolean t1 = in1.hasNextInt();
+        Scanner in2 = new Scanner(arr[2]);
+        boolean t2 = in2.hasNextInt();
+        in1.close();
+        in2.close();
+        if (t1 && t2) {
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[2]);
-        } catch (NumberFormatException e1) {
-            try {
+        } else if (!t1 && !t2)
+        {
             Converting conv1 = Converting.valueOf(arr[0]);
             Converting conv2 = Converting.valueOf(arr[2]);
-            num1 = conv1.getArabic();
-            num2 = conv2.getArabic();
-            isRoman = true;
-            } catch (IllegalArgumentException e2) {
-                return exception;
-            }
-        }
-
+            if ((conv1 instanceof Converting) && (conv2 instanceof Converting)) {
+                num1 = conv1.getArabic();
+                num2 = conv2.getArabic();
+                isRoman = true;
+            } else throw new ArrayIndexOutOfBoundsException();
+        } else throw new IllegalArgumentException();
 
         if ((num1 < 1) || (num1 > 10) || (num2 < 1) || (num2 > 10)) {
-            return exception;
+            throw new IOException();
         }
         String op = arr[1];
 
         int result;
         switch (op) {
             case "+" ->
-                result = num1 + num2;
+                    result = num1 + num2;
 
             case "-" ->
-                result = num1 - num2;
+                    result = num1 - num2;
 
             case "*" ->
-                result = num1 * num2;
+                    result = num1 * num2;
 
             case "/" ->
-                result = num1 / num2;
+                    result = num1 / num2;
 
             default -> {
-                return exception;
+                throw new IOException();
             }
         }
 
@@ -66,7 +70,7 @@ class Main{
 
         if (isRoman){
             if (result < 1){
-                return exception;
+                throw new IOException();
             } else {
                 output = String.valueOf(Converting.values()[result - 1]);
             }
